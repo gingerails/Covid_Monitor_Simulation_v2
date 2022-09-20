056c33bc833c16b34387c362af70d11bb32c24d5
gingerails <rocketcityabi@gmail.com>
2022-09-20 17:49:24 -0500

    still formatting html and css. still need to write more of the simulation. gtg to class now byeee

---
+++ js/classrooms.js
@@ -0,0 +1,9 @@
+
+function multiplyNode(node, count, deep) {
+    for (var i = 0, copy; i < count - 1; i++) {
+        copy = node.cloneNode(deep);
+        node.parentNode.insertBefore(copy, node);
+    }
+}
+
+multiplyNode(document.querySelector('.box-wrapper'), 5, true);
