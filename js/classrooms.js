3172f003da594b3539e378fc13cb914a73b1910b
gingerails <rocketcityabi@gmail.com>
2022-09-20 21:47:14 -0500

    tabs with 3 diff class representations.

--- js/classrooms.js
+++
@@ -1,9 +0,0 @@
-
-function multiplyNode(node, count, deep) {
-    for (var i = 0, copy; i < count - 1; i++) {
-        copy = node.cloneNode(deep);
-        node.parentNode.insertBefore(copy, node);
-    }
-}
-
-multiplyNode(document.querySelector('.box-wrapper'), 5, true);
